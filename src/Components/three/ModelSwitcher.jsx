304f1f92b4258f770a164a885cc650f8f218debf
manish-choudhury <choudhurymanish20@gmail.com>
2026-07-23 19:12:43 +0530

    Showcase section

--- src/Components/three/ModelSwitcher.jsx
+++ src/Components/three/ModelSwitcher.jsx
@@ -32,7 +32,9 @@ const ModelSwitcher = ({scale, isMobile}) => {
     const smallMacbookRef = useRef();
     const largeMacbookRef = useRef();
 
-    const showLargeMacbook = scale === SCALE_LARGE_DESKTOP || SCALE_LARGE_MOBILE;
+    const showLargeMacbook =
+        scale === SCALE_LARGE_DESKTOP ||
+        scale === SCALE_LARGE_MOBILE;
 
     useGSAP(() => {
         if(showLargeMacbook){
