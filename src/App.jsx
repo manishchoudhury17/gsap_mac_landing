ce57cbbc60fc2efdcb35fb5961a78997b51b1ee6
manish-choudhury <choudhurymanish20@gmail.com>
2026-07-23 20:14:58 +0530

    Completed showcase, features, performance, and updated models

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import NavBar from "./Components/NavBar.jsx";
-import Hero from "./Components/Hero.jsx";
-import ProductViewer from "./Components/ProductViewer.jsx";
-import gsap from "gsap";
-import {ScrollTrigger} from "gsap/all";
-import Showcase from "./Components/Showcase.jsx";
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
     return (
@@ -14,7 +18,12 @@ const App = () => {
             <Hero />
             <ProductViewer />
             <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
         </main>
     )
 }
+
 export default App
